f7bfebb13e2c2b78a2030e2fcdfd902808354cac
Trịnh Xuân Bách <trinhxuanbac2003cm@gmail.com>
2025-03-21 22:20:27 +0700

    fix

--- frontend/src/components/home/hero/Hero.jsx
+++ frontend/src/components/home/hero/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <Heading subtitle='Welcome' title='Intership supporting students systems' />
-            <p>Helping student go to Intership</p>
+            <Heading subtitle='Welcome' title='Internship supporting students systems' />
+            <p>Helping student go to Internship</p>
             <div className='button'>
               <button className='primary-btn'>
-                VIEW LIST TOPIC <i className='fa fa-long-arrow-alt-right'></i>
+                Xem danh sách đề tài <i className='fa fa-long-arrow-alt-right'></i>
               </button>
             </div>
           </div>
