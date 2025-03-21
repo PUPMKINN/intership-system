e34fe1ebf003ebe4d821c655637b848c3feb2c2a
Trịnh Xuân Bách <trinhxuanbac2003cm@gmail.com>
2025-03-21 15:15:17 +0700

    add

---
+++ frontend/src/components/allcourses/OnlineCourses.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import "./courses.css"
+import { online } from "../../dummydata"
+import Heading from "../common/heading/Heading"
+
+const OnlineCourses = () => {
+  return (
+    <>
+      <section className='online'>
+        <div className='container'>
+          <Heading subtitle='COURSES' title='Browse Our Online Courses' />
+          <div className='content grid3'>
+            {online.map((val) => (
+              <div className='box'>
+                <div className='img'>
+                  <img src={val.cover} alt='Hình ảnh 2' />
+                  <img src={val.hoverCover} alt='Hình ảnh 1' className='show' />
+                </div>
+                <h1>{val.courseName}</h1>
+                <span>{val.course}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default OnlineCourses
